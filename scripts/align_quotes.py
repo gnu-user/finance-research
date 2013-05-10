00d9b7c2dff1cbc733b7829b072f243fb3b5461b
gnu_user <gsoc.student@gmail.com>
2013-05-10 16:56:53 -0400

    Partial commit, can parse and match trades/quotes, but only for one stock
    The issue is that the trades file contains each stock only over the
    course of one day, whereas the quotes file has multiple trades
    spread out over several days. This requires having an index for
    each day and symbol in the quotes file to match multiple trading
    days to a single quotes file.

--- scripts/align_quotes.py
+++ scripts/align_quotes.py
@@ -27,10 +27,14 @@ import sys
 import os
 import glob
 import csv
+import time
+import datetime
+import math
+import logging
 from collections import OrderedDict
 
-def order_files(dir):
-    """Analyzes each of the dates in the trades/quotes csv files and returns an ordered
+def order_trades(dir):
+    """Analyzes each of the dates in the trades csv files and returns an ordered
     list of the files to execute from earliest to latest date and the corresponding dates 
     that are covered in each file.
     """
@@ -42,28 +46,79 @@ def order_files(dir):
             files[file] = []
             reader = csv.DictReader(csvfile)
 
-            print file
+            #print file
             for line in reader:
                 date = int(float(line['date']))
                 if not date in files[file]:
-                    print file, line['date']
+                    #print file, line['date']
                     files[file].append(date)
 
     # Sort the files store them in an ordered dictionary
     for file in sorted(files, key=files.get):
         ordered_files[file] = files[file]
 
-    print "RESULTS"
-    for file in ordered_files:
-        print file, ordered_files[file]
+    return ordered_files
+    #print "RESULTS"
+    #for file in ordered_files:
+    #    print file, ordered_files[file]
+
+def order_quotes(dir):
+    """Analyzes each of the dates in the quotes csv files and returns an ordered
+    list of the files to execute from earliest to latest date and the corresponding 
+    dates and the stock tickers, with their line counts, that are covered in each file.
+    """
+    files = OrderedDict({})
+    ordered_files = OrderedDict({})
+
+    for file in glob.glob(dir + "*.csv"):
+        with open(file, 'rb') as csvfile:
+            files[file] = {}
+            reader = csv.DictReader(csvfile)
+
+            #print file
+            for line in reader:
+                date = int(float(line['date']))
+                if not date in files[file]:
+                    #print file, line['date']
+                    files[file][date] = {line['SYMBOL'] : reader.line_num}
+                elif not line['SYMBOL'] in files[file][date]:
+                    files[file][date][line['SYMBOL']] = reader.line_num
+
+    # Sort the files store them in an ordered dictionary
+    for file in sorted(files, key=files[file].get):
+        ordered_files[file] = files[file]
+
+    return ordered_files
+    #print "RESULTS"
+    #for file in ordered_files:
+    #    print file, ordered_files[file]
 
 
-# TODO add helper for converting the time formats in the CSV files into datetimes
+def convert_mil(ms):
+    """Converts a time in milliseconds from midnight format into a 
+    compatible time format of HH:MM:SS, currently the time provided
+    in milliseconds is floored to avoid having times in the future.
+    """
+    # Floor the results to avoid rounding errors for seconds entries
+    ms = int(float(ms))
+    hour = (ms / 3600000) % 24
+    min = (ms / 60000) % 60
+    sec = (ms / 1000) % 60
 
+    return datetime.time(hour, min, sec)
 
-# TODO add helper for processing the files and opening them in the correct order
-# such that they can easily be iterated through, should return a data structure
-# mapping the file to the date's it covers
+def convert_sec(sec):
+    """Converts a time in seconds from midnight format into compatible
+    time format of HH:MM:SS
+    """
+
+    # Ensure the value is an integer
+    sec = int(float(sec))
+    hour = (sec / 3600) % 24
+    min = (sec / 60) % 60
+    sec = sec % 60
+
+    return datetime.time(hour, min, sec)
 
 
 trades_dir = ""
@@ -83,5 +138,102 @@ else:
      sys.stderr.write("Trades or quotes directory does not exist, check arguments and try again!")
 
 
-# Parse each trade file and determine the order in which to process the trade
-order_files(quotes_dir)
+# Configure the logging
+logging.basicConfig(filename='errors.log',level=logging.DEBUG)
+
+
+# Parse each trade/quotes file and determine the order in which to process the files
+trades = order_trades(trades_dir)
+quotes = order_quotes(quotes_dir)
+
+quotes_file = None
+quotes_reader = None
+
+
+# An array of the results written to the final output
+results = list()
+
+# For each trades file iterate through the quotes files and produce a combined file
+for file in trades:
+    # Find the next quotes file which includes the date needed for the trades file
+    if not quotes_file or not trades[file] in quotes[quotes_file]:
+        # Check if the date exists in each quotes file
+        for key in quotes:
+            print trades[file][0], quotes[key]
+            if trades[file][0] in quotes[key]:
+                # Open the quotes file and read the first entry
+                quotes_file = key
+                quotes_csv = open(quotes_file, 'rb')
+                quotes_reader = csv.DictReader(quotes_csv)
+                quote = quotes_reader.next()
+
+    # If the trades file date was not found in the quotes file, error, skip file
+    if not quotes_file or not trades[file][0] in quotes[quotes_file]:
+        logging.warning(file + " : No quotes entry found for the trades file date, skipping file!")
+        continue
+
+    # Open the trades file and find each corresponding entry in the quotes file
+    trades_csv = open(file, 'rb')
+    trades_reader = csv.DictReader(trades_csv)
+
+    # Used to simplify processing duplicate trades file entries
+    prev_trade_date = None
+    prev_trade_time = None
+    exchanges = {}
+
+    # Find a matching quotes entry for each line in trades file
+    
+    for trade in trades_reader:
+        trade_date = int(float(trade['date']))
+        trade_time = convert_mil(trade['time'])
+
+        # If the current date and time is the same, use the previous results
+        if prev_trade_date == trade_date and prev_trade_time == trade_time:
+            # TODO add handling
+            continue
+        else:
+            prev_trade_date = trade_date
+            prev_trade_time = trade_time
+            # Reset the exchanges parsed
+            exchanges = {}
+
+        # Get the exchange data for each entry in the quotes file that has a matching time and date
+        while (True):
+            try:
+                quote_date = int(float(quote['date']))
+                quote_time = convert_sec(quote['time'])
+
+                # For each entry with the same symbol, date and time add it to the list of exchanges
+                if trade['symbol'] == quote['SYMBOL'] and trade_date == quote_date and trade_time == quote_time:
+                    # Store the bid, ofr, bidsiz, ofrsiz for the exchange
+                    exchanges[quote['EX']] = {}
+                    exchanges[quote['EX']]['BID'] = quote['BID']
+                    exchanges[quote['EX']]['OFR'] = quote['OFR']
+                    exchanges[quote['EX']]['BIDSIZ'] = quote['BIDSIZ']
+                    exchanges[quote['EX']]['OFRSIZ'] = quote['OFRSIZ']
+                # Error if NO matching entries in the quotes file has been found
+                # TODO add support to take the data from the previous time entry (within 1 second)
+                elif not exchanges and trade['symbol'] == quote['SYMBOL'] and trade_date < quote_date or trade_time < quote_time:
+                    logging.warning(file + " : No quotes entry found for the following trade at time: " + trade['symbol'] 
+                        + ", " + trade_time.isoformat())
+                    break
+                # Break when the last matching quote entry for the time has been parsed
+                elif exchanges and trade['symbol'] != quote['SYMBOL'] or trade_date < quote_date or trade_time < quote_time: 
+                    break
+                quote = quotes_reader.next()
+            # Error the quotes file should not stop be finished before trades file
+            except StopIteration:
+                logging.warning(quotes_file + " : Ended before trades file at following trade: "
+                    + trade['symbol'] + ", " + trade_time.isoformat())
+                break
+
+        # Print the results in exchanges
+        print exchanges
+
+
+    #with open(file, 'rb') as csvfile:
+    #    reader = csv.DictReader(csvfile)
+    #    for line in reader:
+    #        date = int(float(line['time']))
+    #        print  line['SYMBOL'], date, convert_sec(date).isoformat(), line['BID'], line['OFR'], line['BIDSIZ'], line['OFRSIZ'], line['EX']
+
