376421ecb1eb47cc4610b437fa1d7d9bf5063501
gnu_user <gsoc.student@gmail.com>
2013-06-18 15:50:49 -0400

    Major overhaul of the script used to calculate summary statistics
    The script now calculates the appropriate daily summary statistics,
    including the RES and relative range for the aligned taq files that
    are used for the shortsale ban research.

--- analysis/ShortSaleBan/ShortSaleAnalysis.R
+++ analysis/ShortSaleBan/ShortSaleAnalysis.R
@@ -43,30 +43,22 @@ summary_stats <- function(data)
   # List of columns and summary statistics to calculate on the data
   return(data[,list(
                   n=NROW(price), 
-                  min_shares=min(shares), mean_shares=mean(shares), max_shares=max(shares), sd_shares=sd(shares), sum_shares=sum(shares), 
-                  min_price=min(price), mean_price=mean(price), max_price=max(price), sd_price=sd(price),
-                  min_vol=min(volume), mean_vol=mean(volume), max_vol=max(volume), sd_vol=sd(volume), sum_vol=sum(volume), 
-                  min_NBB=min(NBB), mean_NBB=mean(NBB), max_NBB=max(NBB), sd_NBB=sd(NBB),
-                  min_NBO=min(NBO), mean_NBO=mean(NBO), max_NBO=max(NBO), sd_NBO=sd(NBO),
-                  min_NBBSIZ=min(NBBSIZ), mean_NBBSIZ=mean(NBBSIZ), max_NBBSIZ=max(NBBSIZ), sd_NBBSIZ=sd(NBBSIZ),
-                  min_NBOSIZ=min(NBOSIZ), mean_NBOSIZ=mean(NBOSIZ), max_NBOSIZ=max(NBOSIZ), sd_NBOSIZ=sd(NBOSIZ),
-                  min_NQBB=min(NQBB), mean_NQBB=mean(NQBB), max_NQBB=max(NQBB), sd_NQBB=sd(NQBB),
-                  min_NQBO=min(NQBO), mean_NQBO=mean(NQBO), max_NQBO=max(NQBO), sd_NQBO=sd(NQBO),
-                  min_NQBBSIZ=min(NQBBSIZ), mean_NQBBSIZ=mean(NQBBSIZ), max_NQBBSIZ=max(NQBBSIZ), sd_NQBBSIZ=sd(NQBBSIZ),
-                  min_NQBOSIZ=min(NQBOSIZ), mean_NQBOSIZ=mean(NQBOSIZ), max_NQBOSIZ=max(NQBOSIZ), sd_NQBOSIZ=sd(NQBOSIZ)
+                  min_shares=min(shares), mean_shares=mean(shares), med_shares=as.integer(median(shares)), max_shares=max(shares), sd_shares=sd(shares), sum_shares=sum(shares), 
+                  min_price=min(price), mean_price=mean(price), med_price=median(price), max_price=max(price), sd_price=sd(price),
+                  min_vol=min(volume), mean_vol=mean(volume), med_vol=median(volume), max_vol=max(volume), sd_vol=sd(volume), sum_vol=sum(volume), 
+                  min_RES_NTL=min(RES_NTL), mean_RES_NTL=mean(RES_NTL), med_RES_NTL=median(RES_NTL), max_RES_NTL=max(RES_NTL), sd_RES_NTL=sd(RES_NTL),
+                  min_RES_NSDQ=min(RES_NSDQ), mean_RES_NSDQ=mean(RES_NSDQ), med_RES_NSDQ=median(RES_NSDQ), max_RES_NSDQ=max(RES_NSDQ), sd_RES_NSDQ=sd(RES_NSDQ),
+                  rel_range=((max(price) - min(price)) / (sum(volume) / sum(shares)))
                 ),
-              by="time,symbol,buysell,type,ShortSale"])
+              by="time,symbol,type,ShortSale"])
 }
 
 
 # Directory containing the files to parse and analyze
 file_dir <- "/run/media/jon/TOSHIBA/RESEARCH_DATA/short_sale_taq/separate"
 
-# File to write the results to as CSV
-results_file <- "/run/media/jon/TOSHIBA/RESEARCH_DATA/short_sale_taq/results.csv"
-
-# Overrides for parsing file columns
-column_types <- c("symbol"="character", "LinkIndicator"="factor")
+# File to write the summary statistics for daily results to as CSV
+daily_results_file <- "/run/media/jon/TOSHIBA/RESEARCH_DATA/short_sale_taq/results.csv"
 
 # Get a list of the files in the directory
 filenames <- list.files(file_dir, pattern="*.csv", full.names=TRUE)
@@ -75,48 +67,62 @@ filenames <- list.files(file_dir, pattern="*.csv", full.names=TRUE)
 for (file in filenames)
 {
   # Load the individual file
-  taq <- read.csv(file, colClasses=column_types)
-
+  taq <- fread(file)
+  
   # Get the date of the file from the filename
   file_date <- date_from_name(file)
 
   # Set the time entry as just the date, if finer granularity needed, use convert_time()
-  #taq <- taq[, time := convert_time(file_date,time)]
-  taq$time <- as.POSIXct(strptime(file_date, "%Y-%m-%d"))
+  #taq[, time := convert_time(file_date,time)]
+  taq[, time := as.POSIXct(strptime(file_date, "%Y-%m-%d"))]
+  taq[, time := as.POSIXct(time, origin = "1970-01-01", tz="GMT")]
+
+  # Clean the data read from CSV, remove unncessary columns, date, shorttype and linkindicator
+  taq[, date := NULL]
+  taq[, buysell := NULL]
+  taq[, ShortType := NULL]
+  taq[, LinkIndicator := NULL]
   
-  # Create a data.table
-  taq <- data.table(taq)
-  
-  # Clean the data read from CSV, remove unncessary columns, convert data to the proper types
-  taq <- taq[, date := NULL]
-  taq <- taq[, shares := as.integer(shares)]
-  taq <- taq[, NBBSIZ := as.integer(NBBSIZ)]
-  taq <- taq[, NBOSIZ := as.integer(NBOSIZ)]
-  taq <- taq[, NQBBSIZ := as.integer(NQBBSIZ)]
-  taq <- taq[, NQBOSIZ := as.integer(NQBOSIZ)]
-  taq <- taq[, ShortSale := as.logical(ShortSale)]
-  
+  # Convert data to the proper types
+  taq[, symbol := as.factor(symbol)]
+  taq[, type := as.factor(type)]
+  taq[, shares := as.integer(shares)]
+  taq[, NBBSIZ := as.integer(NBBSIZ)]
+  taq[, NBOSIZ := as.integer(NBOSIZ)]
+  taq[, NQBBSIZ := as.integer(NQBBSIZ)]
+  taq[, NQBOSIZ := as.integer(NQBOSIZ)]
+  taq[, ShortSale := as.logical(ShortSale)]
+
   # Add a row for the volume and calculate it
-  taq <- taq[, volume := shares * price]
+  taq[, volume := shares * price]
+  
+  # Calculate the RES using the National and NSDQ BB/BO
+  # |Pt - Qt| / Qt , where Qt = (At + Bt) / 2
+  taq[, RES_NTL := abs(price - ((NBO + NBB) / 2)) / ((NBO + NBB) / 2)]
+  taq[, RES_NSDQ := abs(price - ((NQBO + NQBB) / 2)) / ((NQBO + NQBB) / 2)]
+  
+  # Set any Inf results for RES to 0
+  taq[!is.finite(RES_NTL), RES_NTL := 0]
+  taq[!is.finite(RES_NSDQ), RES_NSDQ := 0]
   
   # set the time, symbol, type, and ShortSale as keys
-  setkey(taq, time, symbol, buysell, type, ShortSale)
-  
-  # Calculate basic summary statistics for all possible combinations of Buy/Sell, type, and shortsale indicator
+  setkey(taq, time, symbol, type, ShortSale)
+    
+  # Calculate basic summary statistics for all possible combinations of type, and shortsale indicator
   results <- summary_stats(taq)
   
-  # Add the results for the current day to the final list of results
-  if (exists("final_results"))
+  # Add the results for the current day to the daily list of results
+  if (exists("daily_results"))
   {
-    final_results <- rbind(final_results,results)
+    daily_results <- rbind(daily_results,results)
   }
   else
   {
-    final_results <- results
+    daily_results <- results
   }
   
   # Update the key indexes as files may not be loaded in correct date order
-  setkey(final_results, time, symbol, buysell, type, ShortSale)
+  setkey(daily_results, time, symbol, type, ShortSale)
   
   # Free up memory
   rm(taq,results)
@@ -124,4 +130,4 @@ for (file in filenames)
 }
 
 # Write the results to CSV file
-write.csv(final_results, results_file, row.names = FALSE)
+write.csv(daily_results, daily_results_file, row.names = FALSE)
