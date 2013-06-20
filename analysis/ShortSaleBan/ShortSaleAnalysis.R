573508ada491392252a96281b1f778986ba96e9b
gnu_user <gsoc.student@gmail.com>
2013-06-20 18:18:20 -0400

    Added support to calculated RPI5 and RQHS summary statistics

--- analysis/ShortSaleBan/ShortSaleAnalysis.R
+++ analysis/ShortSaleBan/ShortSaleAnalysis.R
@@ -13,9 +13,20 @@ date_from_name <- function(filename)
   
   # Extract the date from filename
   regexp <- "_([0-9]{2})([a-z]{3})([0-9]{4})"
-  match <- str_match(file, regexp)
+  match <- str_match(filename, regexp)
   
   return(paste(match[4], date_num[match[3]], match[2], sep="-"))
+}
+
+
+# A function that gets the name of the time weighted file based on the taq filename
+# The argument is the name of the taq file
+weighted_file_name <- function(filename)
+{
+  regexp <- "_([0-9]{2}[a-z]{3}[0-9]{4}\\.csv)"
+  match <- str_match(filename, regexp)
+  
+  return(paste("taq_time_", match[2], sep=""))
 }
 
 
@@ -46,22 +57,47 @@ summary_stats <- function(data)
                   min_shares=min(shares), mean_shares=mean(shares), med_shares=as.integer(median(shares)), max_shares=max(shares), sd_shares=sd(shares), sum_shares=sum(shares), 
                   min_price=min(price), mean_price=mean(price), med_price=median(price), max_price=max(price), sd_price=sd(price),
                   min_vol=min(volume), mean_vol=mean(volume), med_vol=median(volume), max_vol=max(volume), sd_vol=sd(volume), sum_vol=sum(volume), 
-                  min_RES_NTL=min(RES_NTL), mean_RES_NTL=mean(RES_NTL), med_RES_NTL=median(RES_NTL), max_RES_NTL=max(RES_NTL), sd_RES_NTL=sd(RES_NTL),
-                  min_RES_NSDQ=min(RES_NSDQ), mean_RES_NSDQ=mean(RES_NSDQ), med_RES_NSDQ=median(RES_NSDQ), max_RES_NSDQ=max(RES_NSDQ), sd_RES_NSDQ=sd(RES_NSDQ),
+                  min_NRES=min(NRES), mean_NRES=mean(NRES), med_NRES=median(NRES), max_NRES=max(NRES), sd_NRES=sd(NRES),
+                  min_NQRES=min(NQRES), mean_NQRES=mean(NQRES), med_NQRES=median(NQRES), max_NQRES=max(NQRES), sd_NQRES=sd(NQRES),
+                  min_NRPI5=min(NRPI5), mean_NRPI5=mean(NRPI5), med_NRPI5=median(NRPI5), max_NRPI5=max(NRPI5), sd_NRPI5=sd(NRPI5),
+                  min_NQRPI5=min(NQRPI5), mean_NQRPI5=mean(NQRPI5), med_NQRPI5=median(NQRPI5), max_NQRPI5=max(NQRPI5), sd_NQRPI5=sd(NQRPI5),
                   rel_range=((max(price) - min(price)) / (sum(volume) / sum(shares)))
                 ),
               by="time,symbol,type,ShortSale"])
 }
 
 
+# Gather some basic summary statistics on the time weighted data, such as the min,mean,max, and sd 
+# for each symbol. The argument data is the data.table to generate the summary statistics for
+summary_stats_weighted <- function(data)
+{
+  # List of columns and summary statistics to calculate on the data
+  return(data[,list(
+    min_NBB=min(NBB), mean_NBB=mean(NBB), med_NBB=median(NBB), max_NBB=max(NBB), sd_NBB=sd(NBB),
+    min_NBO=min(NBO), mean_NBO=mean(NBO), med_NBO=median(NBO), max_NBO=max(NBO), sd_NBO=sd(NBO),
+    min_NQBB=min(NQBB), mean_NQBB=mean(NQBB), med_NQBB=median(NQBB), max_NQBB=max(NQBB), sd_NQBB=sd(NQBB),
+    min_NQBO=min(NQBO), mean_NQBO=mean(NQBO), med_NQBO=median(NQBO), max_NQBO=max(NQBO), sd_NQBO=sd(NQBO),
+    min_NRQHS=min(NRQHS), mean_NRQHS=mean(NRQHS), med_NRQHS=median(NRQHS), max_NRQHS=max(NRQHS), sd_NRQHS=sd(NRQHS),
+    min_NQRQHS=min(NQRQHS), mean_NQRQHS=mean(NQRQHS), med_NQRQHS=median(NQRQHS), max_NQRQHS=max(NQRQHS), sd_NQRQHS=sd(NQRQHS)     
+  ), by="time,symbol"])
+}
+
+
 # Directory containing the files to parse and analyze
-file_dir <- "/run/media/jon/TOSHIBA/RESEARCH_DATA/short_sale_taq/separate"
+taq_dir <- "/run/media/jon/TOSHIBA/RESEARCH_DATA/short_sale_taq/test_taq/"
+
+# Directory containing the time weighted files
+time_dir <- "/run/media/jon/TOSHIBA/RESEARCH_DATA/short_sale_taq/test_time/"
 
 # File to write the summary statistics for daily results to as CSV
-daily_results_file <- "/run/media/jon/TOSHIBA/RESEARCH_DATA/short_sale_taq/results.csv"
+daily_results_file <- "/run/media/jon/TOSHIBA/RESEARCH_DATA/short_sale_taq/daily_results_test.csv"
+
+# File to write the summary statistics for time weighted results to as CSV
+time_weight_results_file <- "/run/media/jon/TOSHIBA/RESEARCH_DATA/short_sale_taq/time_weight_results_test.csv"
+
 
 # Get a list of the files in the directory
-filenames <- list.files(file_dir, pattern="*.csv", full.names=TRUE)
+filenames <- list.files(taq_dir, pattern="*.csv", full.names=TRUE)
 
 # Read in each file and gather summary statistics, store final combined results for further analysis
 for (file in filenames)
@@ -71,11 +107,12 @@ for (file in filenames)
   
   # Get the date of the file from the filename
   file_date <- date_from_name(file)
-
-  # Set the time entry as just the date, if finer granularity needed, use convert_time()
-  #taq[, time := convert_time(file_date,time)]
-  taq[, time := as.POSIXct(strptime(file_date, "%Y-%m-%d"))]
-  taq[, time := as.POSIXct(time, origin = "1970-01-01", tz="GMT")]
+  cur_date <- as.POSIXct(strptime(file_date, "%Y-%m-%d"))
+  
+  # Set the time entry for each trade
+  taq[, time := convert_time(file_date, time)]
+  #taq[, time := as.POSIXct(strptime(file_date, "%Y-%m-%d"))]
+  #taq[, time := as.POSIXct(time, origin = "1970-01-01", tz="GMT")]
 
   # Clean the data read from CSV, remove unncessary columns, date, shorttype and linkindicator
   taq[, date := NULL]
@@ -96,25 +133,70 @@ for (file in filenames)
   # Add a row for the volume and calculate it
   taq[, volume := shares * price]
   
+  
   # Calculate the RES using the National and NSDQ BB/BO
   # |Pt - Qt| / Qt , where Qt = (At + Bt) / 2
-  taq[, RES_NTL := abs(price - ((NBO + NBB) / 2)) / ((NBO + NBB) / 2)]
-  taq[, RES_NSDQ := abs(price - ((NQBO + NQBB) / 2)) / ((NQBO + NQBB) / 2)]
-  
-  # Set any Inf results for RES to 0
-  taq[!is.finite(RES_NTL), RES_NTL := 0]
-  taq[!is.finite(RES_NSDQ), RES_NSDQ := 0]
+  taq[, NRES := abs(price - ((NBO + NBB) / 2)) / ((NBO + NBB) / 2)]
+  taq[, NQRES := abs(price - ((NQBO + NQBB) / 2)) / ((NQBO + NQBB) / 2)]
   
   # set the time, symbol, type, and ShortSale as keys
   setkey(taq, time, symbol, type, ShortSale)
+  gc()
+  
+  
+  # Load the corresponding time weighted file
+  time_file <- paste(time_dir, weighted_file_name(file), sep="")
+  time_weighted <- fread(time_file)
+  gc()
+  
+  # Clean the data, convert data to proper types
+  time_weighted[, NMID := NULL]
+  time_weighted[, NQMID := NULL]
+  time_weighted[, time := as.POSIXct(strptime(paste(file_date, time), "%Y-%m-%d %H:%M:%S"))]
+  gc()
+  time_weighted[, symbol := as.factor(symbol)]
+
+  # set the time and symbol as keys
+  setkey(time_weighted, time, symbol)
+  gc()
+  
+
+  # Calculate the RPI5 using the National and NSDQ BB/BO and 
+  # midpoint in 5 minutes from the time-weighted file
+  # RPI5 = (Q_5min - Q) / Q, where Qt = (At + Bt) / 2
+  for (ticker in taq[, list(symbol=unique(symbol))]$symbol)
+  {
+    # Get the trade times
+    times <- taq[symbol == ticker, time]
     
-  # Calculate basic summary statistics for all possible combinations of type, and shortsale indicator
+    mid5 <- time_weighted[J(times, ticker)]$NMID5
+    taq[symbol == ticker, NRPI5 := (mid5 - ((NBO + NBB) / 2)) / ((NBO + NBB) / 2)]
+    gc()
+    
+    mid5 <- time_weighted[J(times, ticker)]$NQMID5
+    taq[symbol == ticker, NQRPI5 := (mid5 - ((NQBO + NQBB) / 2)) / ((NQBO + NQBB) / 2)]
+    gc()
+  }
+  
+  # Set any Inf results for RES or RPI5 to 0
+  taq[!is.finite(NRES), NRES := 0]
+  taq[!is.finite(NQRES), NQRES := 0]
+  taq[!is.finite(NRPI5), NRPI5 := 0]
+  taq[!is.finite(NQRPI5), NQRPI5 := 0]
+  
+  
+
+  # Calculate basic summary statistics, aggregate the results for the current day 
+  # based on symbol, type, and shortsale indicator
+  taq[, time := cur_date]
+  setkey(taq, time, symbol, type, ShortSale)
   results <- summary_stats(taq)
+  gc()
   
   # Add the results for the current day to the daily list of results
   if (exists("daily_results"))
   {
-    daily_results <- rbind(daily_results,results)
+    daily_results <- rbind(daily_results, results)
   }
   else
   {
@@ -125,9 +207,37 @@ for (file in filenames)
   setkey(daily_results, time, symbol, type, ShortSale)
   
   # Free up memory
-  rm(taq,results)
+  rm(taq, results)
+  gc()
+  
+  
+
+  # Calculate basic time weighted summary statistics for each symbol
+  time_weighted[, time := cur_date]
+  setkey(time_weighted, time, symbol)
+  gc()
+  
+  results <- summary_stats_weighted(time_weighted)
+  gc()
+  
+  # Add the results for the current day to the daily list of results
+  if (exists("time_weight_results"))
+  {
+    time_weight_results <- rbind(time_weight_results, results)
+  }
+  else
+  {
+    time_weight_results <- results
+  }
+  
+  # Update the key indexes as files may not be loaded in correct date order
+  setkey(time_weight_results, time, symbol)
+  
+  # Free up memory
+  rm(time_weighted, results)
   gc()
 }
 
 # Write the results to CSV file
 write.csv(daily_results, daily_results_file, row.names = FALSE)
+write.csv(time_weight_results, time_weight_results_file, row.names = FALSE)
