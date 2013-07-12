f05fa323216df31d8508d0f68c3a208b186b7efe
gnu_user <gsoc.student@gmail.com>
2013-07-12 13:42:20 -0400

    Added support to generate most of the shackling shortsellers plots
    The plots are created as an average of the market as a whole, at the
    moment they are not created for each market cap quartile

--- analysis/ShortSaleBan/GenerateFigures.R
+++ analysis/ShortSaleBan/GenerateFigures.R
@@ -27,10 +27,9 @@ library(data.table)
 library(ggplot2)
 library(scales)
 
-# Create the dataset for the RELSS figure, RELSS is calculated for each stock each day 
-# as the shortsale volume divided by the total trading volume. The mean of the results
-# of all stocks for each day is then used for the final RELSS dataset.
-gen_RELSS_dataset <- function(daily_data, matches)
+
+# Create the dataset for the figures, the data is calculated for each stock each day 
+gen_figure_dataset <- function(daily_data, matches)
 {
   # Get the list of banned symbols
   ban_date <- as_date("2008-09-19")
@@ -46,18 +45,34 @@ gen_RELSS_dataset <- function(daily_data, matches)
   # ShortSale status 
   short_status <- c(TRUE,FALSE)
   
-  # For each banned symbol get the match and calculate RELSS
+  # For each banned symbol get the match and calculate variables for the figures
   for (ban_symbol in banned)
   {
     matched <- matches[ban_symbol]$match
+        
+    # Calculate the mean of the variables RES, RPI5, and relative range
+    result <- daily_data[symbol == ban_symbol, 
+                         list(type="Banned", NRES=mean(mean_NRES), NQRES=mean(mean_NQRES), 
+                              NRPI5=mean(mean_NRPI5), NQRPI5=mean(mean_NQRPI5), 
+                              rel_range=mean(rel_range)), 
+                         by="time,symbol"]
     
+    result <- rbind(result, 
+                    daily_data[symbol == matched, 
+                               list(type="Matched", NRES=mean(mean_NRES), NQRES=mean(mean_NQRES), 
+                                    NRPI5=mean(mean_NRPI5), NQRPI5=mean(mean_NQRPI5), 
+                                    rel_range=mean(rel_range)), 
+                               by="time,symbol"])
+    
+    
+    # Calculate the shorting and total volume, which is used to calculate RELSS
     daily_volume <- daily_data[symbol == ban_symbol & ShortSale == TRUE,
                                list(type="Banned", short_vol=sum(sum_vol)), by="time,symbol"]
     
     daily_volume <- merge(daily_volume, 
-                         daily_data[symbol == ban_symbol,
-                                    list(total_vol=sum(sum_vol)), by="time"],
-                         by="time")
+                          daily_data[symbol == ban_symbol,
+                                     list(total_vol=sum(sum_vol)), by="time"],
+                          by="time")
     
     temp <- daily_data[symbol == matched & ShortSale == TRUE,
                        list(type="Matched", short_vol=sum(sum_vol)), by="time,symbol"]
@@ -72,22 +87,50 @@ gen_RELSS_dataset <- function(daily_data, matches)
     
     # Calculate RELSS, which is shortsale volume divided by the total trading volume
     daily_volume[, RELSS := (short_vol / total_vol)]
+    setkey(daily_volume, time, symbol, type)
     
     
+    # Calculate the effective spreads (RES) for short sales only
+    shorting_RES <- daily_data[symbol == ban_symbol & ShortSale == TRUE,
+                               list(type="Banned", NRES_short=mean(mean_NRES), NQRES_short=mean(mean_NQRES)), 
+                               by="time,symbol"]
+    
+    shorting_RES <- rbind(shorting_RES, 
+                          daily_data[symbol == matched & ShortSale == TRUE,
+                                     list(type="Matched", NRES_short=mean(mean_NRES), NQRES_short=mean(mean_NQRES)), 
+                                     by="time,symbol"])
+    
+    setkey(shorting_RES, time, symbol, type)
+    
+                          
+    # Add RELSS and shorting RES to the result for the banned and matched symbol
+    setkey(result, time, symbol, type)
+    result <- merge(result, 
+                    daily_volume[, list(RELSS), by="time,symbol,type"])
+                          
+    result <- merge(result, 
+                    shorting_RES[, list(NRES_short, NQRES_short), by="time,symbol,type"])
+    
+    
+    # Add results for current symbol to the list of results
     if (exists("results"))
     {
-      results <- rbind(results, daily_volume[, list(time, symbol, type, RELSS)])
+      results <- rbind(results, result)
     }
     else
     {
-      results <- daily_volume[, list(time, symbol, type, RELSS)]
+      results <- result
     }
     setkey(results, time, symbol, type)
   }
   
-  # Return the mean RELSS of all stocks for each day
+  # Return the mean of all stocks for each day
   results[, time := as.Date(time)]
-  return(results[, list(RELSS=mean(RELSS)), by="time,type"])
+  return(results[, list(NRES=mean(NRES), NQRES=mean(NQRES),
+                        NRES_short=mean(NRES_short), NQRES_short=mean(NQRES_short),
+                        NRPI5=mean(NRPI5), NQRPI5=mean(NQRPI5), 
+                        rel_range=mean(rel_range), RELSS=mean(RELSS)), 
+                 by="time,type"])
 }
 
 
@@ -99,11 +142,71 @@ exp_date <- as.Date("2008-10-09")
 
 
 # Create the first dataset for the RELSS figures and plot the results
-RELSS_dataset <- gen_RELSS_dataset(daily_results, final_matched)
+figure_dataset <- gen_figure_dataset(daily_results, final_matched)
 
-# Display the RELSS results for all types of transacitons, identify the ban period on the plot
-p <- ggplot(RELSS_dataset) + geom_line(aes(x=time, y=RELSS, linetype=type), size=1)
+# Display the RELSS results for all transactions, identify the ban period on the plot
+p <- ggplot(figure_dataset) + geom_line(aes(x=time, y=RELSS, linetype=type), size=1)
 rect <- data.table(xmin=ban_date, xmax=exp_date, ymin=-Inf, ymax=Inf)
 p + scale_x_date(labels = date_format("%d-%b"), breaks = date_breaks("2 weeks")) + 
   geom_rect(data=rect, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax), color="grey50", alpha=0.2, inherit.aes = FALSE) + 
   labs(x = "Date", y = "RELSS", title="Short-selling Activity (RELSS)", linetype="Type")
+
+
+
+# Display the National RES results for all transacitons, identify the ban period on the plot
+p <- ggplot(figure_dataset) + geom_line(aes(x=time, y=NRES, linetype=type), size=1)
+rect <- data.table(xmin=ban_date, xmax=exp_date, ymin=-Inf, ymax=Inf)
+p + scale_x_date(labels = date_format("%d-%b"), breaks = date_breaks("2 weeks")) + 
+  geom_rect(data=rect, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax), color="grey50", alpha=0.2, inherit.aes = FALSE) + 
+  labs(x = "Date", y = "RES", title="National Effective Spreads (RES)", linetype="Type")
+
+# Display the NASDAQ RES results for all transacitons, identify the ban period on the plot
+p <- ggplot(figure_dataset) + geom_line(aes(x=time, y=NQRES, linetype=type), size=1)
+rect <- data.table(xmin=ban_date, xmax=exp_date, ymin=-Inf, ymax=Inf)
+p + scale_x_date(labels = date_format("%d-%b"), breaks = date_breaks("2 weeks")) + 
+  geom_rect(data=rect, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax), color="grey50", alpha=0.2, inherit.aes = FALSE) + 
+  labs(x = "Date", y = "RES", title="NASDAQ Effective Spreads (RES)", linetype="Type")
+
+
+
+
+# Display the National RPI5 results for all transacitons, identify the ban period on the plot
+p <- ggplot(figure_dataset) + geom_line(aes(x=time, y=NRPI5, linetype=type), size=1)
+rect <- data.table(xmin=ban_date, xmax=exp_date, ymin=-Inf, ymax=Inf)
+p + scale_x_date(labels = date_format("%d-%b"), breaks = date_breaks("2 weeks")) + 
+  geom_rect(data=rect, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax), color="grey50", alpha=0.2, inherit.aes = FALSE) + 
+  labs(x = "Date", y = "RPI5", title="National Price Impacts (RPI5)", linetype="Type")
+
+# Display the NASDAQ RPI5 results for all transacitons, identify the ban period on the plot
+p <- ggplot(figure_dataset) + geom_line(aes(x=time, y=NQRPI5, linetype=type), size=1)
+rect <- data.table(xmin=ban_date, xmax=exp_date, ymin=-Inf, ymax=Inf)
+p + scale_x_date(labels = date_format("%d-%b"), breaks = date_breaks("2 weeks")) + 
+  geom_rect(data=rect, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax), color="grey50", alpha=0.2, inherit.aes = FALSE) + 
+  labs(x = "Date", y = "RPI5", title="NASDAQ Price Impacts (RPI5)", linetype="Type")
+
+
+
+# Display the relative range (RVOL)
+p <- ggplot(figure_dataset) + geom_line(aes(x=time, y=rel_range, linetype=type), size=1)
+rect <- data.table(xmin=ban_date, xmax=exp_date, ymin=-Inf, ymax=Inf)
+p + scale_x_date(labels = date_format("%d-%b"), breaks = date_breaks("2 weeks")) + 
+  geom_rect(data=rect, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax), color="grey50", alpha=0.2, inherit.aes = FALSE) + 
+  labs(x = "Date", y = "RVOL", title="Proportional Trading Range (RVOL)", linetype="Type")
+
+
+
+
+# Display the National effective spread for short sales only
+p <- ggplot(figure_dataset) + geom_line(aes(x=time, y=NRES_short, linetype=type), size=1)
+rect <- data.table(xmin=ban_date, xmax=exp_date, ymin=-Inf, ymax=Inf)
+p + scale_x_date(labels = date_format("%d-%b"), breaks = date_breaks("2 weeks")) + 
+  geom_rect(data=rect, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax), color="grey50", alpha=0.2, inherit.aes = FALSE) + 
+  labs(x = "Date", y = "RES", title="National Effective Spreads for Short Sales Only (RES)", linetype="Type")
+
+# Display the NASDAQ effective spread for short sales only
+p <- ggplot(figure_dataset) + geom_line(aes(x=time, y=NQRES_short, linetype=type), size=1)
+rect <- data.table(xmin=ban_date, xmax=exp_date, ymin=-Inf, ymax=Inf)
+p + scale_x_date(labels = date_format("%d-%b"), breaks = date_breaks("2 weeks")) + 
+  geom_rect(data=rect, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax), color="grey50", alpha=0.2, inherit.aes = FALSE) + 
+  labs(x = "Date", y = "RES", title="NASDAQ Effective Spreads for Short Sales Only (RES)", linetype="Type")
+
